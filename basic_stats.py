b11fae5e010f6c00a5711453a9e887ea3747f659
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2022-09-07 21:14:34 -0700

    implemented word stats

--- basic_stats.py
+++ basic_stats.py
@@ -1,16 +1,21 @@
 import sqlite3
 import collections
+import tqdm
 
-def pretty_print(dictionary):
+def pretty_print(dictionary, limit = None):
+    counter = 0
     for k, v in dictionary.items():
         print(f"{k} : {v}")
-
+        counter += 1
+        if limit is not None and counter > limit:
+            return
 
 con = sqlite3.connect("zotero.sqlite")
 cur = con.cursor()
 
-#fulltextItemWords gives you every word in your indexed database
-
+# for use in ignoring common words in analysis
+with open("stopwords.txt", "r") as f:
+    STOPWORDS = [s.split("\n")[0] for s in f]
 
 res = cur.execute("SELECT itemTypeID, typeName from itemTypes")
 item_dict = {k : v for (k, v) in res.fetchall()} #mapping between item type id and actual names of item types
@@ -20,28 +25,41 @@ word_dict = {k : v for (k, v) in res.fetchall()} # mapping between word id and a
 
 # TODO: map from item id to metadata, etc
 
-# res = cur.execute("SELECT wordID from fulltextItemWords")
-# word_dump = [k[0] for k in res.fetchall()]
-# item_counts = {}
-# for item in item_types:
-#     clean_item = item[0]
-#     if item_dict[clean_item] not in item_counts:
-#         item_counts[item_dict[clean_item]] = 0
-#     item_counts[item_dict[clean_item]] += 1
-#
-# print(len(word_dump))
+def get_word_counts(cur, stopwords = None):
+    res = cur.execute("SELECT wordID from fulltextItemWords") #gets all the words in a dump
+    word_dump = [k[0] for k in res.fetchall()]
+    word_counts = {}
+    for word in tqdm.tqdm(word_dump):
+        if word_dict[word] not in word_counts:
+            word_counts[word_dict[word]] = 0
+        word_counts[word_dict[word]] += 1
+    clean_counts = {k : v for k, v in word_counts.items() if (stopwords is None or k not in stopwords) and not k.isnumeric()}
+    sorted_counts = collections.OrderedDict(sorted(clean_counts.items(), key=lambda item: item[1], reverse=True))
+    return sorted_counts
+
+def get_attachment_counts(cur):
+    res = cur.execute("SELECT itemTypeID from items")
+    item_types = [k[0] for k in res.fetchall()]
+    item_counts = {}
+    for clean_item in tqdm.tqdm(item_types):
+        if item_dict[clean_item] not in item_counts:
+            item_counts[item_dict[clean_item]] = 0
+        item_counts[item_dict[clean_item]] += 1
+
+    ordered_item_counts = collections.OrderedDict(sorted(item_counts.items()))
+    return ordered_item_counts
+
+sorted_counts = get_word_counts(cur, STOPWORDS)
+pretty_print(sorted_counts, 100)
+
+while True:
+    s = input("word? ")
+    if s in sorted_counts:
+        print(sorted_counts[s])
 
 # more ideas
-# total pages indexd, most popular words
+# total pages indexed
 
 ## GET STATS ON EACH DOCUMENT TYPE ##
-res = cur.execute("SELECT itemTypeID from items")
-item_types = [k[0] for k in res.fetchall()]
-item_counts = {}
-for clean_item in item_types:
-    if item_dict[clean_item] not in item_counts:
-        item_counts[item_dict[clean_item]] = 0
-    item_counts[item_dict[clean_item]] += 1
 
-ordered_item_counts = collections.OrderedDict(sorted(item_counts.items()))
-pretty_print(ordered_item_counts)
+# pretty_print(ordered_item_counts)
